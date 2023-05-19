45bf9a65cba50f439719febb606f43434569e412
uttamkrshaw <uttamkr5599@gmail.com>
2023-05-19 22:55:30 +0530

    Readjusting Pages

--- sasta_bazar/src/App.js
+++ sasta_bazar/src/App.js
@@ -1,13 +1,10 @@
 import "./App.css";
-import Footer from "./Components/Footer";
-
 import MainRoutes from "./Pages/MainRoutes";
 
 function App() {
   return (
     <div className="App">
       <MainRoutes />
-      <Footer />
     </div>
   );
 }
